bb2d22781a83912770176a7a44a6d1d7caa7594d
Dr Peter Humburg <peter.humburg@mq.edu.au>
2019-05-14 13:14:26 +1000

    Test interactivity

--- svg/js/interactive.js
+++ svg/js/interactive.js
@@ -1,3 +1,44 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
-});
+window.onload = function() {
+  const svg = getSvg();
+  const toggles = svg.getElementsByClassName('toggle');
+  for (const toggle of toggles) {
+    toggle.addEventListener('click', toggleSubtree);
+  }
+};
+
+/**
+ * Toggle the display of sub-trees in the diagram
+ * @param {Event} event The event that triggered the callback.
+ */
+function toggleSubtree(event) {
+  const target = getSubtree(event.target);
+  if (target.style.display === 'block') {
+    target.style.display = 'none';
+  } else {
+    target.style.display = 'block';
+  }
+}
+
+/**
+ * Get embedded SVG object
+ * @return {Object} SVG object
+ */
+function getSvg() {
+  const obj = document.getElementById('test-choice');
+  return obj.contentDocument || obj.contentWindow.document;
+}
+
+/**
+ * Obtain a subtree of the diagram attached to a toggle node
+ * @param {Element} target Target of a user interaction to toggle visibility
+ *    of a subtree.
+ * @return {Element} The subtree attached to the toggle.
+ */
+function getSubtree(target) {
+  const svg = getSvg();
+  while (target.getAttribute('class') !== 'toggle' && target != svg) {
+    target = target.parentElement;
+  }
+  const subId = target.dataset.subtree;
+  return svg.getElementById(subId);
+}
